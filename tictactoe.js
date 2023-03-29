412e92fb1dd3b33c79d8fa4b3e05ab1bbbb5eca8
David Rubio <corba1318@gmail.com>
2023-03-29 13:17:29 +0200

    actualizacion

--- tictactoe.js
+++ tictactoe.js
@@ -8,12 +8,14 @@
  * En total tenemos 9 casillas que van desde la 0 hasta la 8
  */
 let casillas = document.getElementsByClassName("casilla");
-
+let contadores = document.getElementsByClassName("contador");
+let puntuacionX = document.getElementsByClassName("puntuacionX");
+let puntuacionY = document.getElementsByClassName("puntuacionY");
 /**
  * Creamos un array con arrays que contienen todas las combinaciones ganadoras
  * 
  * [0] => [0, 1, 2]
- * [1] => [3, 4, 5]
+ * [1] => [3, 4, 5] f
  * ...
  */
 let combinacionesGanadoras = [
@@ -71,23 +73,117 @@ for(let i = 0; i < casillas.length; i++){
 let turno = true;
 let x = [];
 let y = [];
+let contador=0;
+puntuacionX.textContent = puntuacionX;
+puntuacionY.textContent = puntuacionY;
+
 function agregarFicha(numero){
-    console.log('Has hecho un click en la casilla ' + numero)
     /**
      * Cuando se activa esta funcion por el evento del click
      * es necesario eliminar el click del div
      */
     casillas[numero].removeAttribute('onclick')
-    
 if (turno){
     casillas[numero].textContent = 'X';
-    turno = false;
-    x.push(numero);
+    x.push(numero); 
+    heGanadoConX();
 }else{
     casillas[numero].textContent = 'O';
-    turno = true;
     y.push(numero);
+    heGanadoConY();
 }
+    turno=!turno;
+    contador ++;
+    if (contador == 9 ){
+        empate();
+    }
+    if(heGanadoConY()){
+        puntuacionY ++;
+    }else if(heGanadoConX()){
+        puntuacionX ++;
+    }
+
+}
+function heGanadoConX(){
+    let contX=0;  
+    console.log(x);
+    for (i in combinacionesGanadoras){
+            for (let k = 0 ; k < x.length ; k++){
+                if(combinacionesGanadoras[i].includes(x[k])==true){
+                    contX ++;
+                    if (contX == 3){
+                        confirm("HA GANADO X");
+                        terminar();
+                        contX == 0;
+                    }
+                }
+            }
+        contX =0;          
+    }
+}
+
+function heGanadoConY(){
+    let contY=0; 
+    console.log(y);
+    for (l in combinacionesGanadoras){
+            for (let m = 0 ; m < x.length ; m++){
+                if(combinacionesGanadoras[l].includes(y[m])==true){
+                    contY ++;
+                    if (contY == 3){
+                        confirm("HA GANADO O");
+                        terminar();
+                        contY == 0;
+                    }
+                }
+            }
+        contY =0;          
+    }
+}
+
+function empate(){
+    confirm("TABLAS");
+}
+function terminar(){
+    for (let n=0 ; n < casillas.length; n++){
+        casillas[n].removeAttribute('onclick');
+    }
+
+}
+function reiniciar(numero){
+    for (let i = 0; i < casillas.length; i++) {
+        casillas[i].innerText = ""
+        casillas[i].setAttribute('onclick', `agregarFicha(${i})`);
+    }
+    x = [];
+    y = [];
+    contador=0;
+
+    turno = true
+    /**
+     * Cuando se activa esta funcion por el evento del click
+     * es necesario eliminar el click del div
+     */
+    
+if (turno){
+    casillas[numero].textContent='X';
+    x.push(numero); 
+    heGanadoConX();
+}else{
+    casillas[numero].textContent = 'O';
+    y.push(numero);
+    heGanadoConY();
+}
+    turno=!turno;
+    contador ++;
+    if (contador == 9 ){
+        empate();
+    }
+    if(heGanadoConY()){
+        puntuacionY ++;
+    }else if(heGanadoConX()){
+        puntuacionX ++;
+    }
+
 }
 
 /**
